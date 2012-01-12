369545b3a028f1fc4f0d7eed017fd779e470f12c
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-01-12 17:00:12 -0500

    $ENVIRONMENT

--- environment.rb
+++ environment.rb
@@ -2,4 +2,12 @@ require 'sinatra' unless defined?(Sinatra)
 require 'dm-core'
 require './models.rb'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://db/development.db')
+$ENVIRONMENT == ENV['SINATRA_ENV'] || :development
+
+
+case $ENVIRONMENT
+  when :development
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://db/development.db')
+  when :production
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || fail )
+end
