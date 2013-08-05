db624e4d2a6769662e94a454185a633c2f833b4e
audy <harekrishna@gmail.com>
2013-08-05 14:20:09 -0400

    use classes for spec descriptions

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe 'The Applicaiton App' do
+describe Skellington do
 
   it 'should load the index page successfully' do
     get '/'
