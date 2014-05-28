68e4204d24f06717b507dc248b19b1029d2b3f73
Austin Davis-Richardson <harekrishna@gmail.com>
2014-05-28 09:23:05 -0400

    dry

--- environment.rb
+++ environment.rb
@@ -12,20 +12,17 @@ class Skellington < Sinatra::Base
 
   set :root, File.dirname(__FILE__)
 
-  DB_PATH = File.join(File.dirname(__FILE__), 'db')
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
 
   configure :development do
-    DataMapper.setup(:default, ENV['DATABASE_URL'])
     require 'sinatra/reloader'
     register Sinatra::Reloader
   end
 
   configure :production do
-    DataMapper.setup(:default, ENV['DATABASE_URL'])
   end
 
   configure :test do
-    DataMapper.setup(:default, ENV['DATABASE_URL'])
   end
 
 end
